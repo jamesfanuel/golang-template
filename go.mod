17520289ba6aaac2ad2a64823234214ff28cd37f
jamesfanuelcinema21 <jamesfanuel@cinema21.co.id>
2024-03-08 13:48:38 +0700

    2024-03-08

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module ds3-product-v2
+module x1-schedule
 
 go 1.20
 
@@ -8,7 +8,12 @@ require (
 	github.com/spf13/viper v1.16.0
 )
 
-require github.com/xuanbo/requests v0.0.1 // indirect
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/xuanbo/requests v0.0.1 // indirect
+)
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os" // http-swagger middleware
+	"strings"
+	"time"
 
+	"crypto/md5"
+	"encoding/hex"
+
+	"github.com/go-redis/redis/v8"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/spf13/viper"
-
 	eureka "github.com/xuanbo/eureka-client"
 )
 
@@ -24,6 +31,15 @@ type MyNullString struct {
 	sql.NullString
 }
 
+func newRedisClient(host string, password string) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     host,
+		Password: password,
+		DB:       0,
+	})
+	return client
+}
+
 func (s MyNullString) MarshalJSON() ([]byte, error) {
 	if s.Valid {
 		return json.Marshal(s.String)
@@ -31,18 +47,40 @@ func (s MyNullString) MarshalJSON() ([]byte, error) {
 	return []byte(`null`), nil
 }
 
-type Product struct {
-	ID              int          `json:"id"`
-	ProductCode     string       `json:"product_code"`
-	ProductName     string       `json:"product_name"`
-	ProductType     int          `json:"product_type"`
-	SubCategoryCode MyNullString `json:"subcategory_code"`
-	CategoryCode    MyNullString `json:"category_code"`
-	ActiveStatus    int          `json:"active_status"`
+type Schedule struct {
+	PlayDate    MyNullString `json:"play_date"`
+	TheaterCode MyNullString `json:"theater_code"`
+	City        MyNullString `json:"city"`
+	TheaterName MyNullString `json:"theater_name"`
+	Studio      MyNullString `json:"studio"`
+	MovieCode   MyNullString `json:"movie_code"`
+	MovieName   MyNullString `json:"movie_name"`
+	ShowType    MyNullString `json:"show_type"`
+	ShowTime    MyNullString `json:"show_time"`
+}
+
+type FinalSchedule struct {
+	ScheduleDate string             `json:"schedule_date"`
+	TheaterCode  string             `json:"theater_code"`
+	Studio       string             `json:"studio"`
+	Schedule     []ScheduleByOutlet `json:"schedule"`
+}
+
+type ScheduleByOutlet struct {
+	ShowID    MyNullString `json:"show_id"`
+	ShowTime  MyNullString `json:"show_time"`
+	MovieCode MyNullString `json:"movie_code"`
+	ShowType  MyNullString `json:"show_type"`
+}
+
+type ScheduleMD5 struct {
+	MD5 string `json:"md5"`
 }
 
 var db *sql.DB
 var err error
+
+var ctx = context.Background()
 
 // @title           Swagger Example API
 // @version         1.0
@@ -56,7 +94,7 @@ var err error
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:9211
+// @host      localhost:6001
 // @BasePath  /api/v1
 
 // @securityDefinitions.basic  BasicAuth
@@ -65,9 +103,10 @@ var err error
 // @externalDocs.url          https://swagger.io/resources/open-api/
 func main() {
 	client := eureka.NewClient(&eureka.Config{
-		DefaultZone:           "http://172.16.2.21:8762/eureka/",
-		App:                   "ds3-product-v2",
-		Port:                  9211,
+		DefaultZone: "http://localhost:8763/eureka/",
+		// DefaultZone:           "http://172.16.2.21:8763/eureka/",
+		App:                   "x1-schedule",
+		Port:                  6001,
 		RenewalIntervalInSecs: 10,
 		DurationInSecs:        30,
 		Metadata: map[string]interface{}{
@@ -79,10 +118,11 @@ func main() {
 			"SERVICE_VERSION_CODE": "DEFAULT",
 		},
 	})
+
 	// start client, register、heartbeat、refresh
 	client.Start()
 
-	viper.SetConfigName("dev.yaml")
+	viper.SetConfigName("prod.yaml")
 	viper.AddConfigPath(".")
 	// Enable VIPER to read Environment Variables
 	viper.AutomaticEnv()
@@ -110,6 +150,7 @@ func main() {
 	fmt.Printf("viper : %s = %s \n", "Database Username", DBUsername)
 	fmt.Printf("viper : %s = %s \n", "Database Password", DBPassword)
 	fmt.Printf("viper : %s = %s \n", "Database Name", DBName)
+	fmt.Printf("viper : %s = %s \n", "Service Port", "6001")
 
 	// db, err = sql.Open("mysql", "dsserver:xxi2121.@tcp(k8s.devel.intra.db.cinema21.co.id:3306)/db_digsig")
 	db, err = sql.Open("mysql", DBUsername+":"+DBPassword+"@tcp("+DBHost+":"+DBPort+")/"+DBName)
@@ -119,7 +160,7 @@ func main() {
 	}
 	defer db.Close()
 
-	logfile, err := os.Create("logs/ds3-product-v2.log")
+	logfile, err := os.Create("logs/x1-schedule.log")
 
 	if err != nil {
 		log.Println(err.Error())
@@ -130,139 +171,290 @@ func main() {
 	log.SetOutput(logfile)
 
 	router := mux.NewRouter()
-	router.HandleFunc("/get", getProduct).Methods("GET")
-	router.HandleFunc("/get/{id}", getProductById).Methods("GET")
-	router.HandleFunc("/create", insertProduct).Methods("POST")
-	router.HandleFunc("/update/{id}", updateProduct).Methods("PUT")
-	router.HandleFunc("/delete/{id}", deleteProduct).Methods("DELETE")
-	http.ListenAndServe(":9211", router)
+	router.HandleFunc("/TrafficIntegration", getSchedule).Methods("GET")
+	// router.HandleFunc("/get/{id}", getMemberLogById).Methods("GET")
+	router.HandleFunc("/create", insertSchedule).Methods("POST")
+	http.ListenAndServe(":6001", router)
 }
 
 // @Summary Get Product
 // @Router  /get [GET]
-func getProduct(w http.ResponseWriter, r *http.Request) {
+func getSchedule(w http.ResponseWriter, r *http.Request) {
+	var redisHost = "localhost:6379"
+	var redisPassword = ""
+
+	rdb := newRedisClient(redisHost, redisPassword)
+	fmt.Println("redis client initialized")
+
+	currentTime := time.Now()
+	// get data
+	op2 := rdb.Get(context.Background(), currentTime.Format("2006-01-02")+"_"+strings.Split(r.URL.Query().Get("t"), "_")[0]+"_"+strings.Split(r.URL.Query().Get("t"), "_")[1])
+	if err := op2.Err(); err != nil {
+		fmt.Printf("unable to GET data. error: %v", err)
+		return
+	}
+	res, err := op2.Result()
+	if err != nil {
+		fmt.Printf("unable to GET data. error: %v", err)
+		return
+	}
+	log.Println("get operation success. result:", res)
+
+	fmt.Fprintf(w, res)
+}
+
+// func GetMD5Hash(text string) string {
+// 	hash := md5.Sum([]byte(text))
+// 	return hex.EncodeToString(hash[:])
+// }
+
+// func OnPage(link string) string {
+// 	res, err := http.Get(link)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	content, err := io.ReadAll(res.Body)
+// 	res.Body.Close()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	return string(content)
+// }
+
+func insertSchedule(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now()
+
+	log.Printf("Download File")
+	downloadFile("file/jadwal.txt", "https://m.cinemaxxi.net/ftp-jadwal/xl"+currentTime.Format("060102")+".txt")
+
 	log.Printf("Request from %s for %s", r.RemoteAddr, r.URL)
 	w.Header().Set("Content-Type", "application/json")
-	var products []Product
-	result, err := db.Query("SELECT id, product_code, product_name, product_type, subcategory_code, category_code, active_status from ds_product")
+
+	file, err := os.Open("file/jadwal.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	hash1 := md5.New()
+	_, err = io.Copy(hash1, file)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// hash2 := md5.New()
+	// hash2.Write([]byte(OnPage("https://m.cinemaxxi.net/ftp-jadwal/xl" + currentTime.Format("060102") + ".txt")))
+
+	result, err := db.Query("SELECT value from setting_tbl where varname = 'schedule.md5'")
 	if err != nil {
 		log.Println(err.Error())
 		panic(err.Error())
 	}
 	defer result.Close()
-	for result.Next() {
-		var product Product
-		err := result.Scan(&product.ID, &product.ProductCode, &product.ProductName, &product.ProductType, &product.SubCategoryCode, &product.CategoryCode, &product.ActiveStatus)
-		if err != nil {
-			log.Println(err.Error())
-			panic(err.Error())
-		}
-		products = append(products, product)
-	}
-	json.NewEncoder(w).Encode(products)
-}
 
-// @Summary Get Product By Id
-// @Router  /get/{id} [GET]
-func getProductById(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Request from %s for %s", r.RemoteAddr, r.URL)
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-
-	result, err := db.Query("SELECT id, product_code, product_name, product_type, subcategory_code, category_code, active_status from ds_product where id = ?", params["id"])
-	if err != nil {
-		log.Println(err.Error())
-		panic(err.Error())
-	}
-	defer result.Close()
-
-	var product Product
+	var ScheduleMD5 ScheduleMD5
 
 	for result.Next() {
-		err := result.Scan(&product.ID, &product.ProductCode, &product.ProductName, &product.ProductType, &product.SubCategoryCode, &product.CategoryCode, &product.ActiveStatus)
+		err := result.Scan(&ScheduleMD5.MD5)
 		if err != nil {
 			log.Println(err.Error())
 			panic(err.Error())
 		}
 	}
-	json.NewEncoder(w).Encode(product)
+
+	if hex.EncodeToString(hash1.Sum(nil)) != ScheduleMD5.MD5 {
+
+		file, err := os.Open("file/jadwal.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		del, err := db.Prepare("DELETE FROM temp_schedule")
+		if err != nil {
+			log.Println(err.Error())
+			panic(err.Error())
+		}
+
+		_, err = del.Exec()
+
+		stmt, err := db.Prepare("INSERT INTO temp_schedule (play_date, theater_code, city, theater_name, studio, movie_code, movie_name, show_type, show_time) VALUES(?,?,?,?,?,?,?,?,?)")
+		if err != nil {
+			log.Println(err.Error())
+			panic(err.Error())
+		}
+
+		for scanner.Scan() {
+			theater_code := scanner.Text()[0:7]
+			city := scanner.Text()[7:32]
+			theater_name := scanner.Text()[32:57]
+			studio := scanner.Text()[57:59]
+			movie_code := scanner.Text()[59:65]
+			movie_name := scanner.Text()[65:95]
+			show_type := scanner.Text()[95:97]
+			show_time := scanner.Text()[97:len([]rune(scanner.Text()))]
+			fmt.Println("Insert " + theater_code + "_" + studio + " " + movie_name)
+			start_index := 0
+			end_index := 5
+			for i := 0; i < len([]rune(strings.TrimSpace(show_time)))/5; i++ {
+				_, err = stmt.Exec(currentTime.Format("2006-01-02"), theater_code, city, theater_name, studio, movie_code, movie_name, show_type, show_time[start_index:end_index])
+				start_index += 5
+				end_index += 5
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("Start Insert Into Schedule")
+
+		del2, err := db.Prepare("DELETE FROM schedule")
+		if err != nil {
+			log.Println(err.Error())
+			panic(err.Error())
+		}
+
+		_, err = del2.Exec()
+
+		trn, err := db.Prepare(`INSERT INTO schedule(show_date, cinema_code, screen_no, show_start, show_type, film_row_id, show_no)
+		SELECT play_date
+		, theater_code
+		, studio
+		, ADDTIME(show_time , "00:09") AS show_time
+		, show_type
+		, movie_code
+		, ROW_NUMBER() OVER(PARTITION BY play_date, theater_code, studio ORDER BY play_date, theater_code, studio, show_time) AS show_no
+		FROM temp_schedule
+		ORDER BY play_date, theater_code , studio , show_time`)
+		if err != nil {
+			log.Println(err.Error())
+			panic(err.Error())
+		}
+
+		_, err = trn.Exec()
+
+		if err != nil {
+			log.Println(err.Error())
+			panic(err.Error())
+		}
+
+		log.Printf("Done Insert Into Schedule")
+
+		var redisHost = "localhost:6379"
+		var redisPassword = ""
+
+		rdb := newRedisClient(redisHost, redisPassword)
+		fmt.Println("redis client initialized")
+
+		currentTime = time.Now()
+
+		result, err := db.Query("SELECT show_date, cinema_code, screen_no FROM schedule WHERE show_date = ? GROUP BY show_date, cinema_code, screen_no", currentTime.Format("2006-01-02"))
+		if err != nil {
+			log.Println(err.Error())
+			panic(err.Error())
+		}
+		defer result.Close()
+
+		for result.Next() {
+			var FinalSchedule FinalSchedule
+			var ScheduleByOutlets []ScheduleByOutlet
+
+			err := result.Scan(&FinalSchedule.ScheduleDate, &FinalSchedule.TheaterCode, &FinalSchedule.Studio)
+			if err != nil {
+				log.Println(err.Error())
+				panic(err.Error())
+			}
+
+			result2, err := db.Query(`SELECT
+			CONCAT(replace(show_date,"-",""),".",cinema_code,".",screen_no,".",show_no) AS show_id
+			, CONCAT (show_date, " ", show_start ) AS show_time
+			, film_row_id AS movie_code
+			, show_type
+			FROM
+			schedule WHERE cinema_code = ? AND screen_no = ? AND show_date = ? ORDER BY show_start`, &FinalSchedule.TheaterCode, &FinalSchedule.Studio, currentTime.Format("2006-01-02"))
+
+			if err != nil {
+				log.Println(err.Error())
+				panic(err.Error())
+			}
+
+			defer result2.Close()
+			for result2.Next() {
+				var ScheduleByOutlet ScheduleByOutlet
+				err := result2.Scan(&ScheduleByOutlet.ShowID, &ScheduleByOutlet.ShowTime, &ScheduleByOutlet.MovieCode, &ScheduleByOutlet.ShowType)
+				if err != nil {
+					log.Println(err.Error())
+					panic(err.Error())
+				}
+
+				ScheduleByOutlets = append(ScheduleByOutlets, ScheduleByOutlet)
+			}
+
+			FinalSchedule.Schedule = append(FinalSchedule.Schedule, ScheduleByOutlets...)
+
+			key := currentTime.Format("2006-01-02") + "_" + FinalSchedule.TheaterCode + "_" + FinalSchedule.Studio
+			data, err := json.Marshal(FinalSchedule)
+			ttl := time.Duration(64800) * time.Second
+
+			// store data using SET command
+			op1 := rdb.Set(context.Background(), key, data, ttl)
+			if err := op1.Err(); err != nil {
+				fmt.Printf("unable to SET data. error: %v", err)
+				return
+			}
+
+			fmt.Println("Insert Key " + currentTime.Format("2006-01-02") + "_" + FinalSchedule.TheaterCode + "_" + FinalSchedule.Studio + " to Redis")
+			log.Println("set operation success")
+		}
+
+		json.NewEncoder(w).Encode("Schedule Success Insert to Redis")
+
+		stmt2, err := db.Prepare("UPDATE setting_tbl SET value = ? WHERE varname='schedule.md5'")
+		if err != nil {
+			log.Println(err.Error())
+			panic(err.Error())
+		}
+		_, err = stmt2.Exec(hex.EncodeToString(hash1.Sum(nil)))
+		if err != nil {
+			log.Println(err.Error())
+			panic(err.Error())
+		}
+
+	}
+
+	json.NewEncoder(w).Encode("No Data to Update")
+
 }
 
-func insertProduct(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Request from %s for %s", r.RemoteAddr, r.URL)
-	w.Header().Set("Content-Type", "application/json")
+func downloadFile(filepath string, url string) (err error) {
 
-	stmt, err := db.Prepare("INSERT INTO ds_product (product_code, product_name, product_type, subcategory_code, category_code, product_end_date, active_status, created_time, created_by) VALUES(?,?,?,?,?,?,1,NOW(),?)")
+	// Create the file
+	out, err := os.Create(filepath)
 	if err != nil {
-		log.Println(err.Error())
-		panic(err.Error())
+		return err
 	}
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err.Error())
-		panic(err.Error())
-	}
-	keyVal := map[string]any{}
-	json.Unmarshal(body, &keyVal)
-	product_code := keyVal["product_code"]
-	product_name := keyVal["product_name"]
-	product_type := keyVal["product_type"]
-	subcategory_code := keyVal["subcategory_code"]
-	category_code := keyVal["category_code"]
-	product_end_date := keyVal["product_end_date"]
-	created_by := keyVal["created_by"]
-	_, err = stmt.Exec(product_code, product_name, product_type, subcategory_code, category_code, product_end_date, created_by)
-	if err != nil {
-		log.Println(err.Error())
-		panic(err.Error())
-	}
-	fmt.Fprintf(w, "New product was created")
-}
+	defer out.Close()
 
-func updateProduct(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Request from %s for %s", r.RemoteAddr, r.URL)
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-	stmt, err := db.Prepare("UPDATE ds_product SET product_code=?, product_name=?, product_type=?, subcategory_code=?, category_code=?, product_end_date=?, active_status=1, updated_time=NOW(), updated_by=? WHERE id=?;")
-	if err != nil {
-		log.Println(err.Error())
-		panic(err.Error())
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
 	}
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err.Error())
-		panic(err.Error())
-	}
-	keyVal := map[string]any{}
-	json.Unmarshal(body, &keyVal)
-	new_product_code := keyVal["product_code"]
-	new_product_name := keyVal["product_name"]
-	new_product_type := keyVal["product_type"]
-	new_subcategory_code := keyVal["subcategory_code"]
-	new_category_code := keyVal["category_code"]
-	new_product_end_date := keyVal["product_end_date"]
-	updated_by := keyVal["updated_by"]
-	_, err = stmt.Exec(new_product_code, new_product_name, new_product_type, new_subcategory_code, new_category_code, new_product_end_date, updated_by, params["id"])
-	if err != nil {
-		log.Println(err.Error())
-		panic(err.Error())
-	}
-	fmt.Fprintf(w, "Product with ID = %s was updated", params["id"])
-}
 
-func deleteProduct(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Request from %s for %s", r.RemoteAddr, r.URL)
-	params := mux.Vars(r)
-	stmt, err := db.Prepare("DELETE FROM ds_product WHERE id = ?")
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
 	if err != nil {
-		log.Println(err.Error())
-		panic(err.Error())
+		return err
 	}
-	_, err = stmt.Exec(params["id"])
-	if err != nil {
-		log.Println(err.Error())
-		panic(err.Error())
-	}
-	fmt.Fprintf(w, "Product with ID = %s was deleted", params["id"])
+
+	return nil
 }
